16ad5e4f055e21f130f797506cd4d28fcd24f650
Семен <EmelyanenkoSemyon2006@ya.ru>
2026-01-18 02:20:54 +0300

    Init

---
+++ MessengerDBInit.sql
@@ -0,0 +1,109 @@
+CREATE TYPE genders_type AS ENUM ('Male', 'Female');
+CREATE TYPE system_roles_type AS ENUM ('User', 'Admin');
+
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    surname VARCHAR(100) NOT NULL,
+    second_name VARCHAR(100),
+    date_of_birth DATE,
+    gender genders_type,
+    email_address VARCHAR(260) NOT NULL UNIQUE,
+    phone_number VARCHAR(50) UNIQUE,
+    country VARCHAR(100),
+    city VARCHAR(100),
+    about TEXT,
+    avatar_photo_path VARCHAR(500),
+    login VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    date_and_time_registered TIMESTAMPTZ NOT NULL,
+    messenger_role system_roles_type NOT NULL
+);
+
+CREATE INDEX idx_users_username ON users (UPPER(username));
+CREATE INDEX idx_users_name ON users (UPPER(name));
+CREATE INDEX idx_users_surname ON users (UPPER(surname));
+CREATE INDEX idx_users_second_name ON users (UPPER(second_name));
+CREATE INDEX idx_users_name_surname_second_name ON users (UPPER(name), UPPER(surname), UPPER(second_name));
+
+CREATE TABLE user_friends (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT REFERENCES users ON DELETE CASCADE NOT NULL,
+    friend_user_id BIGINT REFERENCES users ON DELETE CASCADE NOT NULL,
+    date_and_time_added TIMESTAMPTZ NOT NULL,
+    UNIQUE(user_id, friend_user_id),
+    CHECK (user_id != friend_user_id)
+);
+
+CREATE INDEX idx_user_friends_user_id ON user_friends (user_id);
+CREATE INDEX idx_user_friends_friend_user_id ON user_friends (friend_user_id);
+
+CREATE TABLE user_friend_requests (
+    id BIGSERIAL PRIMARY KEY,
+    sender_user_id BIGINT REFERENCES users ON DELETE CASCADE NOT NULL,
+    receiver_user_id BIGINT REFERENCES users ON DELETE CASCADE NOT NULL,
+    date_and_time_sent TIMESTAMPTZ NOT NULL,
+    UNIQUE(sender_user_id, receiver_user_id),
+    CHECK (sender_user_id != receiver_user_id)
+);
+
+CREATE INDEX idx_user_friend_requests_sender_user_id ON user_friend_requests (sender_user_id);
+CREATE INDEX idx_user_friend_requests_receiver_user_id ON user_friend_requests (receiver_user_id);
+
+CREATE TABLE chats (
+    id BIGSERIAL PRIMARY KEY,
+    is_group_chat BOOLEAN NOT NULL,
+    name VARCHAR(100),
+    owner_user_id BIGINT REFERENCES users ON DELETE SET NULL,
+    date_and_time_created TIMESTAMPTZ NOT NULL,
+    avatar_photo_path VARCHAR(500)
+);
+
+CREATE INDEX idx_chats_name ON chats (UPPER(name));
+
+CREATE TABLE chat_users (
+    id BIGSERIAL PRIMARY KEY,
+    chat_id BIGINT REFERENCES chats ON DELETE CASCADE NOT NULL,
+    chat_user_id BIGINT REFERENCES users ON DELETE CASCADE NOT NULL,
+    date_and_time_added TIMESTAMPTZ NOT NULL,
+    is_user_admin BOOLEAN NOT NULL,
+    UNIQUE(chat_id, chat_user_id)
+);
+
+CREATE INDEX idx_chat_users_chat_id ON chat_users (chat_id);
+CREATE INDEX idx_chat_users_user_id ON chat_users (chat_user_id);
+
+CREATE TABLE messages (
+    id BIGSERIAL PRIMARY KEY,
+    chat_id BIGINT REFERENCES chats ON DELETE CASCADE NOT NULL,
+    sender_user_id BIGINT REFERENCES users ON DELETE SET NULL,
+    date_and_time_sent TIMESTAMPTZ NOT NULL,
+    message_text TEXT,
+    message_text_tsvector TSVECTOR GENERATED ALWAYS AS (TO_TSVECTOR('russian', message_text)) STORED
+);
+
+CREATE INDEX idx_messages_chat_id ON messages (chat_id);
+CREATE INDEX idx_messages_chat_id_date_and_time_sent ON messages (chat_id, date_and_time_sent);
+CREATE INDEX idx_messages_message_text_tsvector ON messages USING GIN(message_text_tsvector);
+
+CREATE TABLE file_attachments (
+    id BIGSERIAL PRIMARY KEY,
+    message_id BIGINT REFERENCES messages ON DELETE CASCADE NOT NULL,
+    attachment_file_path VARCHAR(500) NOT NULL,
+    UNIQUE(message_id, attachment_file_path)
+);
+
+CREATE INDEX idx_file_attachments_message_id ON file_attachments (message_id);
+
+CREATE TABLE received_messages (
+    id BIGSERIAL PRIMARY KEY,
+    message_id BIGINT REFERENCES messages ON DELETE CASCADE NOT NULL,
+    received_user_id BIGINT REFERENCES users ON DELETE CASCADE NOT NULL,
+    date_and_time_received TIMESTAMPTZ NOT NULL,
+    UNIQUE(message_id, received_user_id)
+);
+
+CREATE INDEX idx_received_messages_message_id ON received_messages (message_id);
+CREATE INDEX idx_received_messages_received_user_id ON received_messages (received_user_id);
+CREATE INDEX idx_received_messages_message_id_received_user_id ON received_messages (message_id, received_user_id);
---
+++ backend/database.py
@@ -0,0 +1,125 @@
+import enum
+from datetime import datetime, date
+
+import sqlalchemy.dialects.postgresql
+import sqlalchemy.orm
+
+
+class Gender(enum.Enum):
+    male = "Male"
+    female = "Female"
+
+
+class SystemRoles(enum.Enum):
+    user = "User"
+    admin = "Admin"
+
+
+class Base(sqlalchemy.orm.DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, primary_key=True)
+    username: sqlalchemy.orm.Mapped[str] = sqlalchemy.orm.mapped_column(sqlalchemy.VARCHAR(100), nullable=False)
+    name: sqlalchemy.orm.Mapped[str] = sqlalchemy.orm.mapped_column(sqlalchemy.VARCHAR(100), nullable=False)
+    surname: sqlalchemy.orm.Mapped[str] = sqlalchemy.orm.mapped_column(sqlalchemy.VARCHAR(100), nullable=False)
+    second_name: sqlalchemy.orm.Mapped[str] = sqlalchemy.orm.mapped_column(sqlalchemy.VARCHAR(100), nullable=False)
+    date_of_birth: sqlalchemy.orm.Mapped[date | None] = sqlalchemy.orm.mapped_column(sqlalchemy.Date)
+    gender: sqlalchemy.orm.Mapped[Gender | None] = sqlalchemy.orm.mapped_column(sqlalchemy.Enum(Gender))
+    email_address: sqlalchemy.orm.Mapped[str] = sqlalchemy.orm.mapped_column(sqlalchemy.VARCHAR(260), nullable=False)
+    phone_number: sqlalchemy.orm.Mapped[str] = sqlalchemy.orm.mapped_column(sqlalchemy.VARCHAR(50), nullable=False)
+    country: sqlalchemy.orm.Mapped[str| None] = sqlalchemy.orm.mapped_column(sqlalchemy.VARCHAR(100))
+    city: sqlalchemy.orm.Mapped[str| None] = sqlalchemy.orm.mapped_column(sqlalchemy.VARCHAR(100))
+    about: sqlalchemy.orm.Mapped[str| None] = sqlalchemy.orm.mapped_column(sqlalchemy.Text)
+    avatar_photo_path: sqlalchemy.orm.Mapped[str| None] = sqlalchemy.orm.mapped_column(sqlalchemy.VARCHAR(500))
+    login: sqlalchemy.orm.Mapped[str] = sqlalchemy.orm.mapped_column(sqlalchemy.VARCHAR(100), nullable=False)
+    password_hash: sqlalchemy.orm.Mapped[str] = sqlalchemy.orm.mapped_column(sqlalchemy.VARCHAR(255), nullable=False)
+    date_and_time_registered: sqlalchemy.orm.Mapped[datetime] = sqlalchemy.orm.mapped_column(sqlalchemy.TIMESTAMP(timezone=True), nullable=False)
+    messenger_role: sqlalchemy.orm.Mapped[SystemRoles] = sqlalchemy.orm.mapped_column(sqlalchemy.Enum(SystemRoles), nullable=False)
+    __table_args__ = (sqlalchemy.UniqueConstraint('username', name="users_username_key"),
+                      sqlalchemy.UniqueConstraint('email_address', name="users_email_address_key"),
+                      sqlalchemy.UniqueConstraint('phone_number', name="users_phone_number_key"),
+                      sqlalchemy.UniqueConstraint('login', name="users_login_key"),
+                      sqlalchemy.Index('idx_users_username', 'UPPER(username)'),
+                      sqlalchemy.Index('idx_users_name', 'UPPER(name)'),
+                      sqlalchemy.Index('idx_users_surname', 'UPPER(surname)'),
+                      sqlalchemy.Index('idx_users_second_name', 'UPPER(second_name)'),
+                      sqlalchemy.Index('idx_users_name_surname_second_name', 'UPPER(name)', 'UPPER(surname)', 'UPPER(second_name)'))
+
+
+class UserFriend(Base):
+    __tablename__ = 'user_friends'
+    id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, primary_key=True, autoincrement=True)
+    user_id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, sqlalchemy.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    friend_user_id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, sqlalchemy.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    date_and_time_added: sqlalchemy.orm.Mapped[datetime] = sqlalchemy.orm.mapped_column(sqlalchemy.TIMESTAMP(timezone=True), nullable=False)
+    __table_args__ = (
+        sqlalchemy.UniqueConstraint('user_id', 'friend_user_id', name='user_friends_user_id_friend_user_id_key'),
+        sqlalchemy.CheckConstraint("user_id != friend_user_id"),
+        sqlalchemy.Index('idx_user_friends_user_id', 'user_id'),
+        sqlalchemy.Index('idx_user_friends_friend_user_id', 'friend_user_id'))
+
+
+class UserFriendRequest(Base):
+    __tablename__ = 'user_friend_requests'
+    id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, primary_key=True, autoincrement=True)
+    sender_user_id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, sqlalchemy.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    receiver_user_id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, sqlalchemy.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    date_and_time_sent: sqlalchemy.orm.Mapped[datetime] = sqlalchemy.orm.mapped_column(sqlalchemy.TIMESTAMP(timezone=True), nullable=False)
+    __table_args__ = (sqlalchemy.UniqueConstraint('sender_user_id', 'receiver_user_id', name='user_friend_requests_sender_user_id_receiver_user_id_key'),
+                      sqlalchemy.CheckConstraint("sender_user_id != receiver_user_id"),
+                      sqlalchemy.Index('idx_user_friend_requests_sender_user_id', 'sender_user_id'),
+                      sqlalchemy.Index('idx_user_friend_requests_receiver_user_id', 'receiver_user_id'))
+
+
+class Chat(Base):
+    __tablename__ = 'chats'
+    id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, primary_key=True, autoincrement=True)
+    is_group_chat: sqlalchemy.orm.Mapped[bool] = sqlalchemy.orm.mapped_column(sqlalchemy.BOOLEAN, nullable=False)
+    name: sqlalchemy.orm.Mapped[str | None] = sqlalchemy.orm.mapped_column(sqlalchemy.VARCHAR(100))
+    owner_user_id: sqlalchemy.orm.Mapped[int | None] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, sqlalchemy.ForeignKey('users.id', ondelete='SET NULL'), nullable=True)
+    date_and_time_created: sqlalchemy.orm.Mapped[datetime] = sqlalchemy.orm.mapped_column(sqlalchemy.TIMESTAMP(timezone=True), nullable=False)
+    avatar_photo_path: sqlalchemy.orm.Mapped[str | None] = sqlalchemy.orm.mapped_column(sqlalchemy.VARCHAR(500))
+    __table_args__ = (sqlalchemy.Index('idx_chats_name', 'UPPER(name)', ))
+
+
+class ChatUser(Base):
+    __tablename__ = 'chat_users'
+    id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, primary_key=True, autoincrement=True)
+    chat_id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, sqlalchemy.ForeignKey('chats.id', ondelete='CASCADE'), nullable=False)
+    chat_user_id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, sqlalchemy.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    date_and_time_added: sqlalchemy.orm.Mapped[datetime] = sqlalchemy.orm.mapped_column(sqlalchemy.TIMESTAMP(timezone=True), nullable=False)
+    is_user_admin: sqlalchemy.orm.Mapped[bool] = sqlalchemy.orm.mapped_column(sqlalchemy.BOOLEAN, nullable=False)
+    __table_args__ = (sqlalchemy.UniqueConstraint('chat_id', 'chat_user_id', name='chat_users_chat_id_chat_user_id_key'),)
+
+
+class Message(Base):
+    __tablename__ = 'messages'
+    id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, primary_key=True, autoincrement=True)
+    chat_id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, sqlalchemy.ForeignKey('chats.id', ondelete='CASCADE'), nullable=False)
+    sender_user_id: sqlalchemy.orm.Mapped[int | None] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, sqlalchemy.ForeignKey('users.id', ondelete='SET NULL'), nullable=True)
+    date_and_time_sent: sqlalchemy.orm.Mapped[datetime] = sqlalchemy.orm.mapped_column(sqlalchemy.TIMESTAMP(timezone=True), nullable=False)
+    message_text: sqlalchemy.orm.Mapped[str| None] = sqlalchemy.orm.mapped_column(sqlalchemy.TEXT)
+    message_text_tsvector = sqlalchemy.orm.mapped_column(sqlalchemy.dialects.postgresql.TSVECTOR, sqlalchemy.Computed("TO_TSVECTOR('russian', message_text)", persisted=True))
+    __table_args__ = (sqlalchemy.Index("idx_messages_chat_id", "chat_id"),
+                      sqlalchemy.Index('idx_messages_chat_id_date_and_time_sent', 'chat_id', 'date_and_time_sent'),
+                      sqlalchemy.Index('idx_messages_message_text_tsvector', 'message_text_tsvector', postgresql_using='gin'))
+
+
+class FileAttachment(Base):
+    __tablename__ = 'file_attachments'
+    id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, primary_key=True, autoincrement=True)
+    message_id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, sqlalchemy.ForeignKey('messages.id', ondelete='CASCADE'), nullable=False)
+    attachment_file_path: sqlalchemy.orm.Mapped[str] = sqlalchemy.orm.mapped_column(sqlalchemy.VARCHAR(500), nullable=False)
+    __table_args__ = (sqlalchemy.UniqueConstraint('message_id', 'attachment_file_path', name='file_attachments_message_id_attachments_file_path_key'),)
+
+
+class ReceivedMessage(Base):
+    __tablename__ = 'received_messages'
+    id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, primary_key=True, autoincrement=True)
+    message_id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, sqlalchemy.ForeignKey('messages.id', ondelete='CASCADE'), nullable=False)
+    received_user_id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.BIGINT, sqlalchemy.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    date_and_time_received: sqlalchemy.orm.Mapped[datetime] = sqlalchemy.orm.mapped_column(sqlalchemy.TIMESTAMP(timezone=True), nullable=False)
+    __table_args__ = (sqlalchemy.UniqueConstraint('message_id', 'received_user_id', name='received_messages_message_id_received_user_id_key'),)
